541ebcd6b6c1e5b3138a92f4ddb8c92177e6573c
Polaco01 <vitor.santana.alves@escola.pr.gov.br>
2024-09-11 19:32:50 -0300

    n

---
+++ polaco.js
@@ -0,0 +1,22 @@
+let cor
+let ty;
+let px;
+let py;
+function setup() (
+createCanvas(600, 450);
+background("awhite");
+px= (200);
+py = (200)
+3
+function draw() (
+tx = (random(0, 50))
+ty = (random(0, 50))
+cor = color(random(0, 255)
+random(0,255),£random(0, 255));
+if(mouseIsPressed)(
+fill(cor);
+circle(px, ру, 30)
+px= mousex
+py = mouseY
+3
+ر
